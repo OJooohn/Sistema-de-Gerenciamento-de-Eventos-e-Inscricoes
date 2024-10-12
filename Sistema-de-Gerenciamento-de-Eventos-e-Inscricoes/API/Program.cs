bbcf2fb3d4cb0733a1846e88661a5ee3e737e208
John Chappell <john.claude.cameron@gmail.com>
2024-10-12 00:43:54 -0300

    Update Program.cs

--- Sistema-de-Gerenciamento-de-Eventos-e-Inscricoes/API/Program.cs
+++ Sistema-de-Gerenciamento-de-Eventos-e-Inscricoes/API/Program.cs
@@ -1,6 +1,9 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello, Minimal API!");
 
 app.Run();
