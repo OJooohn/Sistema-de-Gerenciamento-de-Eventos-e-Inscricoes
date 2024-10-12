95469c22d877858bb4d3ae7fe8b4cb330fbbd5aa
John Chappell <john.claude.cameron@gmail.com>
2024-10-12 01:02:28 -0300

    CREATE and DELETE Usuario

--- Sistema-de-Gerenciamento-de-Eventos-e-Inscricoes/API/Program.cs
+++ Sistema-de-Gerenciamento-de-Eventos-e-Inscricoes/API/Program.cs
@@ -1,9 +1,32 @@
+using API.Models;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDbContex>();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello, Minimal API!");
+
+app.MapPost("/sistema/register", ([FromBody] Usuario usuario, [FromServices] AppDbContex ctx) => {
+
+    ctx.Add(usuario);
+    ctx.SaveChanges();
+    return Results.Created("", usuario);
+
+});
+
+app.MapDelete("/sistema/delete/{id}", (int id, [FromServices] AppDbContex ctx) => {
+
+    Usuario? usuario = ctx.Usuarios.Find(id);
+    if (usuario == null)
+        return Results.NotFound();
+    
+    ctx.Remove(usuario);
+    ctx.SaveChanges();
+    return Results.Ok("Usuário deletado com sucesso!");
+
+});
 
 app.Run();
